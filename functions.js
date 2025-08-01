b20721363e8a1f23562a56db3dae0a138eb5061c
valeriyas903 <valeriyas903@gmail.com>
2025-08-01 17:53:43 +0300

    добавлена игра камень-ножницы-бумага, упр js

--- functions.js
+++ functions.js
@@ -1,133 +1,78 @@
-let arr1 = [1, 5, 4, 10, 0, 3];
+const people = [
+   { name: 'Глеб', age: 29 },
+   { name: 'Анна', age: 17 },
+   { name: 'Олег', age: 7 },
+   { name: 'Оксана', age: 47 }
+];
 
-for (let i = 0; i < arr1.length; i++) {
-  console.log(arr1[i]);
-  if (arr1[i] === 10) {
-    break;
-  }
+console.log(people.sort((a, b) => a.age - b.age));
+
+
+function isPositive(number) {
+    return number > 0;
+}
+
+function isMale(person) {
+    return person.gender === 'male';
+}
+
+function filter(arr, ruleFunction) {
+    const result = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (ruleFunction(arr[i])) {
+            result.push(arr[i]);
+        }
+    }
+    return result;
 }
 
 
+console.log(filter([3, -4, 1, 9], isPositive)); 
 
-let arr2 = [1, 5, 4, 10, 0, 3];
+const names = [
+   {name: 'Глеб', gender: 'male'},
+   {name: 'Анна', gender: 'female'},
+   {name: 'Олег', gender: 'male'},
+   {name: 'Оксана', gender: 'female'}
+];
 
-console.log(arr2.indexOf(4));
-
-
-
-let arr3 = [1, 3, 5, 10, 20];
-
-console.log(arr3.join(' '));
-
+console.log(filter(people, isMale)); 
 
 
-let matrix = [];
 
-for (let i = 0; i < 3; i++) {
-  let row = [];
-  for (let j = 0; j < 3; j++) {
-    row.push(1);
-  }
-  matrix.push(row);
+
+const intervalId = setInterval(() => {
+  console.log(new Date()); 
+}, 3000);
+
+
+setTimeout(() => {
+  clearInterval(intervalId); 
+  console.log('30 секунд прошло');
+}, 30000);
+
+
+
+function delayForSecond(callback) {
+    setTimeout(callback, 1000); 
 }
 
-console.log(matrix);
+delayForSecond(function () {
+   console.log('Привет, Глеб!');
+});
 
 
 
-let arr4 = [1, 1, 1];
-
-arr4.push(2, 2, 2);
-
-console.log(arr4);
-
-
-let arr5 = [9, 8, 7, 'a', 6, 5];
-
-arr5 = arr5.sort().filter(item => typeof item === 'number');
-
-console.log(arr5);
-
-
-let arr6 = [9, 8, 7, 6, 5];
-
-let userInput = prompt("Угадайте число из массива:");
-if (arr6.includes(Number(userInput))) {
-  alert("Угадал");
-} else {
-  alert("Не угадал");
+function delayForSecond(cb) {
+    setTimeout(() => {
+        console.log('Прошла одна секунда');
+        if(cb) {  cb(); }
+    }, 1000)
 }
 
 
-let str = 'abcdef';
-
-console.log(str.split('').reverse().join(''));
-
-
-const arr7 = [[1, 2, 3], [4, 5, 6]];
-const flatArr = [...arr[0], ...arr[1]];
-console.log(flatArr);
-
-
-
-const arr = [3, 7, 1, 9, 4, 6]; 
-
-for (let i = 0; i < arr.length - 1; i++) {
-  const sum = arr[i] + arr[i + 1];
-  console.log(`Сумма элементов ${arr[i]} и ${arr[i + 1]}: ${sum}`);
+function sayHi (name) {
+    console.log(`Привет, ${name}!`);
 }
 
-
-function getSquares(arr) {
-  return arr.map(num => num * num);
-}
-
-const numbers = [1, 2, 3, 4, 5];
-const squares = getSquares(numbers);
-console.log(squares); 
-
-
-function getWordLengths(words) {
-  return words.map(word => word.length);
-}
-
-const strings = ["apple", "banana", "kiwi"];
-const lengths = getWordLengths(strings);
-console.log(lengths);
-
-
-function getNegativeNumbers(arr) {
-  return arr.filter(num => num < 0);
-}
-
-const num = [3, -1, 0, -7, 5, -2];
-const negatives = getNegativeNumbers(numbers);
-console.log(negatives); 
-
-
-const originalArray = [];
-for (let i = 0; i < 10; i++) {
-  const randomNum = Math.floor(Math.random() * 11); 
-  originalArray.push(randomNum);
-}
-
-const evenArray = originalArray.filter(num => num % 2 === 0);
-
-console.log("Исходный массив:", originalArray);
-console.log("Чётные значения:", evenArray);
-
-
-
-
-const numb = [];
-for (let i = 0; i < 6; i++) {
-  const randomNum = Math.floor(Math.random() * 10) + 1; // 1–10 включительно
-  numbers.push(randomNum);
-}
-
-const sum = numbers.reduce((acc, num) => acc + num, 0);
-
-const average = sum / numbers.length;
-
-console.log("Сгенерированный массив:", numbers);
-console.log("Среднее арифметическое:", average);
+delayForSecond(() => sayHi('Глеб'))
